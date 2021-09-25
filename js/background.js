34dc2b60173eb1ec4f3912bf27699aed4063288c
NaRae-tech <narae.dev@gmail.com>
2021-09-25 15:30:21 +0900

    to do list css modify

--- js/background.js
+++ js/background.js
@@ -2,6 +2,7 @@ const images = ["cat1.jpg","cat2.jpg","cat3.jpg","dog1.jpg","dog2.jpg","fox1.jpg
 const chosenImamge = images[Math.floor(Math.random()*images.length)];
 
 const bgImage = document.createElement("img");
+bgImage.className = "animals";
 bgImage.src=`img/${chosenImamge}`;
 
-document.body.appendChild(bgImage);
+document.body.querySelector(".wrap").appendChild(bgImage);
